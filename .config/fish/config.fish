e0461a359b18c0d5a32993e2ef208e14ba11117c
Genadi Samokovarov <gsamokovarov@gmail.com>
2014-01-05 15:54:26 +0200

    Define aliases in fish and autocomplete them
    Another change on the side is mapping the BROWSER environment variable
    to open, so command line applications trying to open a browser can do
    that with the default OSX browser.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -23,6 +23,56 @@ set -x LC_CTYPE en_US.UTF-8
 # just use less for them.
 set -x EDITOR vim
 set -x PAGER less
+set -x BROWSER open
+
+# Create completion for an alias.
+function make_completion --argument alias command
+  complete -c $alias -a "(
+    set -l cmd (commandline -op);
+    set -e cmd[1];
+    complete -C\"$command \$cmd\";
+  )"
+end
+
+# Rage quit everywhere.
+alias Q exit
+
+# Force the regular Vim to open multiple files in tabs.
+alias vim 'vim -p'
+
+# Some systems may not be happy with the $TERM of screen-256colors, so be sane.
+alias ssh "TERM xterm ssh"
+
+# I use vim a lot, so shortcut it to just v.
+alias v vim
+
+# Don't even bother with old vi.
+alias vi vim
+
+# Same goes for rake, grunt, make and git.
+alias r rake
+alias u grunt
+alias m make
+alias d docker
+alias g git
+
+# Make completions for the most commonly used aliases.
+make_completion g 'git'
+make_completion ssh 'ssh'
+make_completion v 'vim -p'
+make_completion vi 'vim -p'
+make_completion vim 'vim -p'
+
+function j
+  cd (autojump $argv)
+end
+
+# Sudo apt-get as it is always what you want.
+alias apt-get "sudo apt-get"
+
+# I never learned how to type freakin' killall.
+alias kilalll killall
+alias kilall killall
 
 # Use the custom solarized LS colors.
 if which dircolors > /dev/null ^&1
