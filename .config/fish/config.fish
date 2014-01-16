023178c0308a935701991b03e05dfa9ad9ed8aac
Genadi Samokovarov <gsamokovarov@gmail.com>
2014-01-16 18:31:23 +0100

    Display ~ while in $HOME in the fish shell prompt

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -51,6 +51,7 @@ end
 
 function fish_right_prompt
   echo -n (set_color -o)(basename $PWD)
+  echo -n (set_color -o)(test $PWD = $HOME; and echo '~'; or basename $PWD)
   if eval (command git rev-parse --is-inside-work-tree ^/dev/null; or echo false)
     echo -n (set_color -o red)"  "(set_color -o cyan)(__fish_git_prompt "%s")
   end
