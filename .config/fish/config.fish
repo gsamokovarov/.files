53404f3837597f567e8bb921eee3a4e6e8ab5d04
Genadi Samokovarov <gsamokovarov@gmail.com>
2014-01-22 22:43:55 +0200

    Extract the git fish shell function into its own file
    Fish autoloads the functions in ~/.config/fish/functions.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -98,7 +98,7 @@ function git
       # are not defined during the first run of the git function. Just inline
       # them for now.
       set -l available_branches (command git branch --no-color -a ^/dev/null | sgrep -v ' -> ' | sed -e 's/^..//' -e 's/^remotes\///')
-      set -l available_tags (command git tag)
+      set -l available_tags (command git tag ^/dev/null)
 
       if contains $target_branch $available_branches $available_tags
         command git checkout $target_branch
