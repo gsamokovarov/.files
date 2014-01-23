e53a3bf3489a685a1df45497d8b566adf63cb427
Genadi Samokovarov <gsamokovarov@gmail.com>
2014-01-23 19:09:44 +0200

    Extract the fish shell aliases in their own file

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -43,47 +43,5 @@ if which dircolors > /dev/null ^&1
   test -f ~/.dir_colors; and . (echo 'set -x '(dircolors ~/.dir_colors | head -1) | psub)
 end
 
-# Aliases
-# -------
-
-# Dot aliases.
-alias .. "cd .."
-alias ... "cd ../.."
-alias .... "cd ../../.."
-alias ..... "cd ../../../.."
-alias ...... "cd ../../../../.."
-
-# Rage quit everywhere.
-alias Q exit
-
-# Sudo apt-get as it is always what you want.
-alias apt-get "sudo apt-get"
-
-# I never learned how to type freakin' killall.
-alias kilalll killall
-alias kilall killall
-
-# Some systems may not be happy with the $TERM of screen-256colors, so be sane.
-alias ssh "env TERM=xterm command ssh"
-
-# I use vim a lot, so shortcut it to just v. On top of that, don't bother with
-# vi if I mistype it and open multiple files in tabs.
-alias v vim;        complete_like v 'vim -p'
-alias vi vim;       complete_like vi 'vim -p'
-alias vim 'vim -p'; complete_like vim 'vim -p'
-
-# Shortcuts for rake, grunt, make, docker and git.
-alias r rake;   complete_like r 'rake'
-alias u grunt;  complete_like u 'grunt'
-alias m make;   complete_like m 'make'
-alias d docker; complete_like d 'docker'
-alias g git;    complete_like g 'git'
-
-# Git subcommands shortcuts.
-alias gc 'git commit';         complete_like gc 'git commit'
-alias gd 'git diff';           complete_like gd 'git diff'
-alias gl 'git log';            complete_like gl 'git log'
-alias ga 'git commit --amend'; complete_like ga 'git commit';
-
-# Just don't, OK?
-alias vag vagrant; complete_like vag 'vagrant'
+# Source the aliases in ~/.config/fish/aliases.fish.
+test -f ~/.config/fish/aliases.fish; and . ~/.config/fish/aliases.fish
