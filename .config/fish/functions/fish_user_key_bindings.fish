5723b52d6ec5f2ccd59b511d325f30574b9d32a7
Genadi Samokovarov <gsamokovarov@gmail.com>
2014-05-02 20:07:45 +0300

    Use Ctrl-X to bring back foreground processes

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -3,4 +3,8 @@ function fish_user_key_bindings
   # search for, then you press the Up arrow. Rebind it to ^R, so I can remember
   # it better.
   bind \cr history-search-backward
+
+# Bring a background process to the foreground witn Ctrl-X. Ctrl-Z cannot be
+# bound due to https://www.mail-archive.com/fish-users@lists.sourceforge.net/msg03480.html
+  bind \cx 'fg; commandline -f repaint'
 end
