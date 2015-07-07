37008e3f1aab3956a13588accea1153e0f315723
Genadi Samokovarov <gsamokovarov@gmail.com>
2015-07-07 19:53:54 +0300

    Keep relative bin as the first in PATH
    Avoids collisions with rbenv shims.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,13 +6,13 @@ set PATH /usr/local/homebrew/bin $PATH
 # preferences on OSX.
 set PATH ~/bin (brew --prefix)"/opt/coreutils/libexec/gnubin" /usr/local/bin /usr/local/sbin $PATH
 
+# Use my own .rbenv, I don't like what Boxen is doing.
+set PATH ~/.rbenv/bin ~/.rbenv/shims $PATH
+
 # Prepend the relative ./bin, so I can get binstubs and the like in the PATH
 # automatically. The node modules binaries are also handy to have around. I
 # don't like installing global node packages.
 set PATH ./bin ./node_modules/.bin $PATH
-
-# Use my own .rbenv, I don't like what Boxen is doing.
-set PATH ~/.rbenv/bin ~/.rbenv/shims $PATH
 
 # Don't show the greeting message on fish boot.
 set -x fish_greeting ''
