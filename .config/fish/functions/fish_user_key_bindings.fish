0417e0522112042ea6b566899097676cd22e13f9
Genadi Samokovarov <gsamokovarov@gmail.com>
2015-07-24 16:22:35 +0300

    Reverse history for fish with fzf

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,8 +1,6 @@
 function fish_user_key_bindings
-  # The reverse search in fish works in reverse. You first type what you want to
-  # search for, then you press the Up arrow. Rebind it to ^R, so I can remember
-  # it better.
-  bind \cr history-search-backward
+  # Search the reverse history with fzf.
+  bind \cr 'history | fzf -s; commandline -f repaint'
 
   # Bring a background process to the foreground witn Ctrl-X. Ctrl-Z cannot be
   # bound due to https://www.mail-archive.com/fish-users@lists.sourceforge.net/msg03480.html
