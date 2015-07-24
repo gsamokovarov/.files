2720f3c32ccaa26e4817a4eb230c29665372f563
Genadi Samokovarov <gsamokovarov@gmail.com>
2015-07-24 14:51:40 +0300

    Use fish shell abbreviations for suitable aliases
    The new abbreviations in fish are pretty cool. When you use an
    abbreviations for an alias, it gives you completion for free.
    
    Still, aliases are useful for different names.

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -20,33 +20,33 @@ alias ssh "env TERM=xterm command ssh"
 
 # I use vim a lot, so shortcut it to just v. On top of that, don't bother with
 # vi if I mistype it and open multiple files in tabs.
-alias v vim;        complete_like v 'vim -p'
-alias vi vim;       complete_like vi 'vim -p'
+abbr -a v=vim
+abbr -a vi=vim
 
 # Shortcuts for rake, grunt, make, docker and git.
-alias r rake;   complete_like r 'rake'
-alias u grunt;  complete_like u 'grunt'
-alias m make;   complete_like m 'make'
-alias d docker; complete_like d 'docker'
-alias g git;    complete_like g 'git'
+abbr -a r=rake
+abbr -a u=grunt
+abbr -a m=make
+abbr -a d=docker
+abbr -a g=git
 
 # Git subcommands shortcuts.
-alias gc 'git commit';         complete_like gc 'git commit'
-alias gco 'git commit';        complete_like gco 'git commit'
-alias gcp 'git cherry-pick';   complete_like gcp 'git cherry-pick'
-alias gd 'git diff';           complete_like gd 'git diff'
-alias gl 'git log';            complete_like gl 'git log'
-alias gs 'git state';          complete_like gs 'git state'
-alias ga 'git commit --amend'; complete_like ga 'git commit'
+abbr -a gc='git commit'
+abbr -a gco='git commit'
+abbr -a gcp='git cherry-pick'
+abbr -a gd='git diff'
+abbr -a gl='git log'
+abbr -a gs='git state'
+abbr -a ga='git commit --amend'
 
 # Yes, that happened today. Age, you know...
 alias mdkir mkdir
 
 # Bundle shortcuts.
-alias be 'bundle exec'; complete_like be 'bundle exec'
+abbr -a be='bundle exec'
 
 # Just don't, OK?
-alias vag vagrant; complete_like vag 'vagrant'
+abbr -a vag vagrant
 
 # Use hub for git with a twist -- if the first argument is an existing branch
 # then switch to it.
