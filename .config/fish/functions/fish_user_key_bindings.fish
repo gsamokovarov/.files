e925d8caedc1d4e9e98a7e1427ed22eeb3a36bcc
Genadi Samokovarov <gsamokovarov@gmail.com>
2015-08-03 19:52:46 +0300

    Don't setup custom fzf on Ctrl-R, use the builtin scripts

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,7 +1,4 @@
 function fish_user_key_bindings
-  # Search the reverse history with fzf.
-  bind \cr 'history | fzf -s; commandline -f repaint'
-
   # Bring a background process to the foreground witn Ctrl-X. Ctrl-Z cannot be
   # bound due to https://www.mail-archive.com/fish-users@lists.sourceforge.net/msg03480.html
   bind \cx 'fg; commandline -f repaint'
