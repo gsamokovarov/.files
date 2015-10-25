99fac64fe2ca11f5e000568285a9463d8444a7d9
Genadi Samokovarov <gsamokovarov@gmail.com>
2015-10-25 11:57:15 +0200

    No more sgrep usage
    After a fish HEAD update, it stopped working. Don't care enough to
    investigate ATM.

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -65,7 +65,7 @@ function git
       # For some reason, the __fish_git_branches and __fish_git_tags functions
       # are not defined during the first run of the git function. Just inline
       # them for now.
-      set -l available_branches (command git branch --no-color -a ^/dev/null | sgrep -v ' -> ' | sed -e 's/^..//' -e 's/^remotes\///')
+      set -l available_branches (command git branch --no-color -a ^/dev/null | grep -v ' -> ' | sed -e 's/^..//' -e 's/^remotes\///')
       set -l available_tags (command git tag ^/dev/null)
 
       if contains $target_branch $available_branches $available_tags
