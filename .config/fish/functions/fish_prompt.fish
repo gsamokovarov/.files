01f61517cc4a88c7ef51d40cfcecf226bcd45a74
Genadi Samokovarov <gsamokovarov@gmail.com>
2016-07-11 18:57:46 +0100

    Use grey for the fish git branch prompt part

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -10,7 +10,7 @@ end
 function fish_right_prompt
   echo -n (set_color -o grey)(test $PWD = $HOME; and echo '~'; or basename $PWD)
   if eval (command git rev-parse --is-inside-work-tree ^/dev/null; or echo false)
-    echo -n (set_color -o red)"  "(set_color -o cyan)(__fish_git_prompt "%s")
+    echo -n (set_color -o red)"  "(set_color -o grey)(__fish_git_prompt "%s")
   end
   set_color normal
 end
