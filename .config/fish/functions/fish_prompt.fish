d82edc765d8b4ca011a3d35504e8172c563d0d52
Genadi Samokovarov <gsamokovarov@gmail.com>
2018-07-27 00:16:56 +0300

    Try out a new, more spaced-out and left-centered prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,16 +1,15 @@
 function fish_prompt
-  if test $status -eq 0
-    echo -n (set_color -o black)"♡  "
-  else
-    echo -n (set_color red)"♡  "
+  echo
+  echo -n (set_color -o black)(test $PWD = $HOME; and echo '~'; or echo $PWD)
+  if eval (command git rev-parse --is-inside-work-tree ^/dev/null; or echo false)
+    echo (set_color -o red)"  "(set_color -o black)(__fish_git_prompt "%s")
   end
   set_color normal
-end
 
-function fish_right_prompt
-  echo -n (set_color -o black)(test $PWD = $HOME; and echo '~'; or basename $PWD)
-  if eval (command git rev-parse --is-inside-work-tree ^/dev/null; or echo false)
-    echo -n (set_color -o red)"  "(set_color -o black)(__fish_git_prompt "%s")
+  if test $status -eq 0
+    echo -n (set_color -o black)"› "
+  else
+    echo -n (set_color -o red)"› "
   end
   set_color normal
 end
