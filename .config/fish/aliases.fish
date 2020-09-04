acce7b68791e143bfe1bd246c8384bf5ac039fff
Genadi Samokovarov <gsamokovarov@gmail.com>
2020-09-04 17:24:00 +0300

    Abbreviate gl to `git short`

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -58,7 +58,7 @@ abbr -a gc git commit
 abbr -a gco git commit
 abbr -a gcp git cherry-pick
 abbr -a gd git diff
-abbr -a gl git log
+abbr -a gl git short
 abbr -a gs git status
 abbr -a ga git commit --amend
 
