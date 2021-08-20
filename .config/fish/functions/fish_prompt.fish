6670366844f047accebf05a95553b24c10e31b58
Genadi Samokovarov <gsamokovarov@gmail.com>
2021-08-20 18:58:21 +0300

    Remove iTerm2 specific coloring hacks in the fish prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,14 +1,14 @@
 function fish_prompt
   echo
-  echo -n (set_color -o black)(test $PWD = $HOME; and echo '~'; or echo (basename $PWD))
+  echo -n (set_color -o white)(test $PWD = $HOME; and echo '~'; or echo (basename $PWD))
   if eval (command git rev-parse --is-inside-work-tree 2&>/dev/null; or echo false)
-    echo -n (set_color -o red)"  "(set_color -o black)(__fish_git_prompt "%s")
+    echo -n (set_color -o red)"  "(set_color -o white)(__fish_git_prompt "%s")
   end
   echo
   set_color normal
 
   if test $status -eq 0
-    echo -n (set_color -o black)"-> "
+    echo -n (set_color -o white)"-> "
   else
     echo -n (set_color -o red)"-> "
   end
