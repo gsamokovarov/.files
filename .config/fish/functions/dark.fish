883b7758276c90cb395ca14cb748a213de550a42
Genadi Samokovarov <gsamokovarov@gmail.com>
2021-08-21 09:57:13 +0300

    Apply {light,dark} across all kitty windows

--- .config/fish/functions/dark.fish
+++ .config/fish/functions/dark.fish
@@ -1,10 +1,21 @@
 function dark
   set -x BACKGROUND dark
 
-  echo "dark" > ~/.colorscheme
+  echo $BACKGROUND > ~/.colorscheme
 
-  kitty @ set-colors ~/.config/kitty/gruvbox_dark.conf
+  # Notify NeoVim for the color change.
   for pid in (pgrep nvim)
     kill -SIGUSR1 $pid
   end
+
+  for id in (kitty @ ls | jq '.[] | .tabs[].windows[].id')
+    # Change the kitty colorscheme for every window.
+    kitty @ set-colors --match id:$id --configured ~/.config/kitty/gruvbox_$BACKGROUND.conf
+
+    # Focus NeoVim so the changes actually take effect.
+    kitty @ focus-window --match id:$id
+  end
+
+  # Focus back the current kitty window that issued the command.
+  kitty @ focus-window --match id:$KITTY_WINDOW_ID
 end
