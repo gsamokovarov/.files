eb0907400806ce2a19a5dd6f641604890d87bba2
Genadi Samokovarov <gsamokovarov@gmail.com>
2021-09-28 13:02:36 +0300

    Let darkmode work when called outside a kitty window

--- .config/fish/functions/darkmode.fish
+++ .config/fish/functions/darkmode.fish
@@ -1,7 +1,7 @@
 function darkmode --argument preference
   set -l background "light"
   if test -z $preference
-    defaults read -g AppleInterfaceStyle &>/dev/null
+    defaults read -g AppleInterfaceStyle 2>/dev/null
     if test $status -eq 0
       set background "dark"
     end
@@ -13,8 +13,10 @@ function darkmode --argument preference
 
   echo $BACKGROUND > ~/.colorscheme
 
+  set -l kitty_socket unix:(echo /tmp/mykitty-*)
+
   # Change the kitty colorscheme for every window.
-  kitty @ set-colors --all --configured ~/.config/kitty/gruvbox_$BACKGROUND.conf
+  kitty @ --to $kitty_socket set-colors --all --configured ~/.config/kitty/gruvbox_$BACKGROUND.conf
 
   # Notify NeoVim for the color change.
   for pid in (pgrep nvim)
@@ -23,7 +25,7 @@ function darkmode --argument preference
 
   # Focus NeoVim so the changes actually take effect. Bring back the focus to
   # the current window ($KITTY_WINDOW_ID) at the end.
-  for id in (kitty @ ls | jq '.[] | .tabs[].windows[].id') $KITTY_WINDOW_ID
-    kitty @ focus-window --match id:$id
+  for id in (kitty @ --to $kitty_socket ls | jq '.[] | .tabs[].windows[].id') $KITTY_WINDOW_ID
+    kitty @ --to $kitty_socket focus-window --match id:$id
   end
 end
