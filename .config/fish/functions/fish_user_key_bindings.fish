3156b8331debba6d081a4ecb4a041baf8bc2ce07
Genadi Samokovarov <gsamokovarov@gmail.com>
2023-10-26 16:58:53 +0300

    Fix a typo

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,5 +1,5 @@
 function fish_user_key_bindings
-  # Bring a background process to the foreground witn Ctrl-X. Ctrl-Z cannot be
+  # Bring a background process to the foreground with Ctrl-X. Ctrl-Z cannot be
   # bound due to https://www.mail-archive.com/fish-users@lists.sourceforge.net/msg03480.html
   bind \cx 'fg; commandline -f repaint'
 end
