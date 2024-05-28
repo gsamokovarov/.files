c77cf7b4f82ab2efb83dbc134dcb3e55e28cf6f0
Genadi Samokovarov <gsamokovarov@gmail.com>
2024-05-28 10:27:31 +0300

    Navigate WezTerm panes with ⌘+{up,down,left,right}arrows

--- .wezterm.lua
+++ .wezterm.lua
@@ -9,14 +9,10 @@ config.color_scheme = 'Gruvbox (Gogh)'
 config.keys = {
   { key = 'd', mods = 'CMD', action = wezterm.action { SplitHorizontal = { domain = 'CurrentPaneDomain' } } },
   { key = 'd', mods = 'CMD|SHIFT', action = wezterm.action { SplitVertical = { domain = 'CurrentPaneDomain' } } },
-  { key = 'h', mods = 'CMD', action = wezterm.action { ActivatePaneDirection = 'Left' } },
-  { key = 'l', mods = 'CMD', action = wezterm.action { ActivatePaneDirection = 'Right' } },
-  { key = 'k', mods = 'CMD', action = wezterm.action { ActivatePaneDirection = 'Up' } },
-  { key = 'j', mods = 'CMD', action = wezterm.action { ActivatePaneDirection = 'Down' } },
-  { key = 'LeftArrow', mods = 'CMD', action = wezterm.action.AdjustPaneSize { 'Left', 1 } },
-  { key = 'RightArrow', mods = 'CMD', action = wezterm.action.AdjustPaneSize { 'Right', 1 } },
-  { key = 'UpArrow', mods = 'CMD', action = wezterm.action.AdjustPaneSize { 'Up', 1 } },
-  { key = 'DownArrow', mods = 'CMD', action = wezterm.action.AdjustPaneSize { 'Down', 1 } },
+  { key = 'LeftArrow', mods = 'CMD', action = wezterm.action { ActivatePaneDirection = 'Left' } },
+  { key = 'RightArrow', mods = 'CMD', action = wezterm.action { ActivatePaneDirection = 'Right' } },
+  { key = 'UpArrow', mods = 'CMD', action = wezterm.action { ActivatePaneDirection = 'Up' } },
+  { key = 'DownArrow', mods = 'CMD', action = wezterm.action { ActivatePaneDirection = 'Down' } },
 }
 
 return config
