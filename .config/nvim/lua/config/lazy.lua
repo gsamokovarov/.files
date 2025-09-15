8b3e686bd8e78a16c2711cfef1fbc94efbd70a0b
Genadi Samokovarov <gsamokovarov@gmail.com>
2025-09-15 12:25:00 +0300

    Default lazyvim to gruvbox
    If no theme is set by omarchy, or overridden by me in other machines,
    default it to gruvbox.

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -17,7 +17,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = { colorscheme = "gruvbox" } },
     -- import/override with your plugins
     { import = "plugins" },
   },
