2d35e4222514ffc82d1371a4cf2c198cc4c67976
Genadi Samokovarov <gsamokovarov@gmail.com>
2025-11-17 09:58:03 +0200

    Don't pin the catopuccin theme no more

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,4 @@
 return {
   -- Gruvbox is _always_ installed.
   { "ellisonleao/gruvbox.nvim", },
-
-  -- See https://github.com/LazyVim/LazyVim/issues/6355.
-  {
-    "catppuccin/nvim",
-    tag = "v1.10.0"
-  },
 }
