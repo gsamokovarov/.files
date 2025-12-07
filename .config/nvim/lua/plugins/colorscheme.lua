f8ac5f1a68df156c16e6aa84f6debb611a5e0fc3
Genadi Samokovarov <gsamokovarov@gmail.com>
2025-12-07 10:53:28 +0200

    Drop catpuccin altogether, let gazarchy link it

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,21 +1,4 @@
 return {
   -- Gruvbox is _always_ installed.
   { "ellisonleao/gruvbox.nvim", },
-
-  -- See https://github.com/LazyVim/LazyVim/issues/6355.
-  {
-    "catppuccin/nvim",
-    opts = {
-      integrations = {
-        bufferline = true,
-      },
-    },
-    specs = {
-      {
-        "akinsho/bufferline.nvim",
-        optional = true,
-        opts = {},
-      },
-    },
-  },
 }
